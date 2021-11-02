5e0dae0bf99f63310ed161d2bab9f8d69daa8606
Arina Dorofeeva <kuriyummy@mail.ru>
2021-11-02 21:47:32 +0300

    load salamander images + refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,4 @@
-import { createClient } from 'pexels';
-
-let a = $("#show_hide_password a");
-let input = $('#show_hide_password input');
-let i = $('#show_hide_password i');
-let a1 = $("#show_hide_password_confirm a");
-let input1 = $('#show_hide_password_confirm input');
-let i1 = $('#show_hide_password_confirm i');
-
+// login & registration
 function eye(a, input, i) {
     a.on('click', function (event) {
         event.preventDefault();
@@ -22,44 +14,38 @@ function eye(a, input, i) {
     });
 }
 
-eye(a, input, i);
-eye(a1, input1, i1);
-
-
-$(document).ready(function () {
-    $("#searchInput").on("keyup", function () {
-        let value = $(this).val().toLowerCase();
-        $("#videoTable tr").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+// my page
+function search() {
+    $(document).ready(function () {
+        $("#searchInput").on("keyup", function () {
+            let value = $(this).val().toLowerCase();
+            $("#videoTable tr").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
         });
     });
-});
-
-
-let imagesPost = document.getElementById("imagesPost");
-
-function fetch() {
-    const client = createClient('563492ad6f91700001000001623d28b6e2b2462b938cc0a3cd1ca298');
-    const query = 'Nature';
-    return client.photos.search({query, per_page: 1});
-
-    //let response = await fetch(`https://jsonplaceholder.typicode.com/photos`)
-    //return await response.json();
 }
 
-function loadPost() {
-    fetch()
-        .then((post) => {
-            for (let index = 0; index < 10; index++) {
+// images
+let imagesPost = document.getElementById("imagesPost");
+
+function loadPost(query) {
+    fetch("https://api.pexels.com/v1/search?query=" + query, {
+        headers: {
+            Authorization: "563492ad6f91700001000001623d28b6e2b2462b938cc0a3cd1ca298"
+        }
+    })
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data => {
+            console.log(data)
+            for (let index = 0; index < data.photos.length; index++) {
                 imagesPost.innerHTML +=
-                    "<div class='bg-dark text-white border border-white card col-3 mx-auto'>" +
-                    "<img class='class-img-top' src='" + post[index].original + "'>" +
-                    "<p class='card-title'>" + post[index].photographer + "</p></div>"
+                    "<img class='mt-3 mb-3 col-3 mx-auto' src='" + data.photos[index].src.original + "'>"
             }
             console.log(imagesPost.innerHTML);
         }).catch((error) => {
         console.log("ERROR: " + error);
-    })
+    });
 }
-
-loadPost();
